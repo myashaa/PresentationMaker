7b872d75c6e611779c9a72f54653914632a6d535
Konstantin <k.podyganov@mail.ru>
2021-10-16 15:39:14 +0300

    добавлены кнопки для управления

--- src/components/actions/ActionBar.tsx
+++ src/components/actions/ActionBar.tsx
@@ -3,5 +3,26 @@ import React from "react";
 import "./ActionBar.styles.css";
 
 export function ActionBar() {
-  return <div className="app-action-bar"></div>;
+  return (
+    <div className="app-action-bar">
+      <ActionButton icon="add" label="Добавить слайд" primary />
+      <ActionButton icon="undo" />
+      <ActionButton icon="redo" />
+    </div>
+  );
+}
+
+type ActionButtonProps = {
+  label?: string;
+  icon?: string;
+  primary?: boolean;
+};
+
+export function ActionButton({ label, icon, primary }: ActionButtonProps) {
+  return (
+    <button className={`action-button ${primary && "primary"}`}>
+      {icon && <span className="material-icons">{icon}</span>}
+      {label && <span className="action-button-label">{label}</span>}
+    </button>
+  );
 }
