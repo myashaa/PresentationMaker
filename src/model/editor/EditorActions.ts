a8cf18eec2a7b4dc4435484767e24d7c7b614c5d
darzo28 <darzo28@yandex.ru>
2021-10-17 16:28:01 +0300

    Добавила реализацию части функций редактора

---
+++ src/model/editor/EditorActions.ts
@@ -0,0 +1,76 @@
+import {Editor} from "./EditorTypes"
+import {Presentation} from "../presentation/PresentationTypes"
+//import {createSlide} from "../presentation/PresentationActions"
+
+export function loadPresentation(file: File): Editor {
+  //TODO: parsing json
+  //const newEditor = {};
+
+  return newEditor;
+}
+
+export function savePresentation(presentation: Presentation) {
+
+}
+
+export function exportPresentation(presentation: Presentation) {
+
+}
+ 
+export function renamePresentation(editor: Editor, name: string): Editor {
+  const newEditor: Editor = {
+    ...editor,
+  };
+
+  newEditor.presentation.name = name;
+
+  return newEditor;
+}
+ 
+export function createPresentation(editor: Editor): Editor {
+  const newEditor: Editor = {
+    ...editor,
+  };
+
+  newEditor.presentation = {
+    name: "New Presentation",
+    slideList: [],
+    selectedSlidesIds: [],
+    selectedElementIds: [],
+  };
+
+  //TODO: newEditor = createSlide(newEditor);
+
+  return newEditor;
+}
+
+export function undo(editor: Editor): Editor {
+  const newEditor = {
+    ...editor,
+  };
+
+  //TODO: undo
+
+  return newEditor;
+}
+
+export function redo(editor: Editor): Editor {
+  const newEditor = {
+    ...editor,
+  };
+
+  //TODO: redo
+
+  return newEditor;
+}
+
+export function changeMode(editor: Editor, mode: "view" | "edit"): Editor {
+  const newEditor = {
+    ...editor,
+  };
+
+  newEditor.mode = mode;
+  
+  return newEditor;
+}
+
