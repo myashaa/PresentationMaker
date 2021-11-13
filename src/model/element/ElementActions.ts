4bd097502c5c869d6b617428b52f3c5565d01664
Konstantin <52314985+kyborq@users.noreply.github.com>
2021-11-13 15:59:39 +0300

    added fns

--- src/model/element/ElementActions.ts
+++ src/model/element/ElementActions.ts
@@ -1,38 +1,53 @@
 export function changeElementBorder(
-  editor: Editor, 
-  slideId: number, 
-  element: SlideElement, 
+  editor: Editor,
+  slideId: number,
+  elementId: number,
   border: Border
-) {
-
-}
+): Editor {}
 
 export function changeElementColor(
-  editor: TextEncoderEncodeIntoResult,
-) {
+  editor: Editor,
+  slideId: number,
+  element: SlideElement,
+  color: string
+): Editor {}
 
-}
+export function setFontFamily(
+  editor: Editor,
+  slideId: number,
+  element: SlideElement,
+  fontFamily: string
+): Editor {}
 
-export function setFontFamily() {
+export function setFontColor(
+  editor: Editor,
+  slideId: number,
+  element: SlideElement,
+  color: string
+): Editor {}
 
-}
+export function setFontSize(
+  editor: Editor,
+  slideId: number,
+  element: SlideElement,
+  size: number
+): Editor {}
 
-export function setFontColor() {
+export function setFilter(
+  editor: Editor,
+  slideId: number,
+  element: SlideElement,
+  filter: string
+): Editor {}
 
-}
+export function deleteFilter(
+  editor: Editor,
+  slideId: number,
+  element: SlideElement
+): Editor {}
 
-export function setFontSize() {
-
-}
-
-export function setFilter() {
-
-}
-
-export function deleteFilter() {
-
-}
-
-export function loadImage() {
-
-}
+export function loadImage(
+  editor: Editor,
+  element: SlideElement,
+  image: Image
+): Editor {}
