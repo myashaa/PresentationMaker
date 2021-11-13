1d6461328434304c5d29eaa4d8683638bb77dce7
Konstantin <52314985+kyborq@users.noreply.github.com>
2021-11-13 14:47:33 +0300

    added fns

---
+++ src/model/element/ElementActions.ts
@@ -0,0 +1,38 @@
+export function changeElementBorder(
+  editor: Editor, 
+  slideId: number, 
+  element: SlideElement, 
+  border: Border
+) {
+
+}
+
+export function changeElementColor(
+  editor: TextEncoderEncodeIntoResult,
+) {
+
+}
+
+export function setFontFamily() {
+
+}
+
+export function setFontColor() {
+
+}
+
+export function setFontSize() {
+
+}
+
+export function setFilter() {
+
+}
+
+export function deleteFilter() {
+
+}
+
+export function loadImage() {
+
+}
