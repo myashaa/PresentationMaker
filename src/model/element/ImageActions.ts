1249039092fb8c29e10fd9add006ffb56cd01370
darzo28 <darzo28@yandex.ru>
2021-11-26 09:34:47 +0300

    #18 Реализовала функции добавления\удаления фильтра

--- src/model/element/ImageActions.ts
+++ src/model/element/ImageActions.ts
@@ -1,27 +1,62 @@
 import { Editor } from "../editor/EditorTypes";
 import { Element, Filter, Image } from "./ElementTypes";
 
-// export function setFilter(
-//   editor: Editor,
-//   slideId: number,
-//   element: Element,
-//   filter: Filter
-// ): Editor {
-//   const data = element.data as Image;
-//   const newData = { ...data, filter };
-//   const newElement = { ...element, data: newData };
+export function setFilter(
+  editor: Editor,
+  slideId: number,
+  element: Element,
+  filter: Filter
+): Editor {
+  //const data = element.data as Image;
+  const data = element.image;
+  const newData = { ...data, filter };
+  const newElement = { ...element, data: newData };
 
-//   const { slideList } = editor.presentation;
-//   const newSlideList = slideList.map((slide, index) =>
-//     index === slideId ? { ...slide, elementList: newElementList } : slide
-//   );
-// }
+  const { slideList } = editor.presentation;
+  const { elementList } = slideList[slideId];
 
-// export function deleteFilter(
-//   editor: Editor,
-//   slideId: number,
-//   element: Element
-// ): Editor {}
+  const newElementList = elementList.map((element, index) =>
+    index === newElement.id ? newElement : element
+  );
+
+  const newSlideList = slideList.map((slide, index) =>
+    index === slideId ? { ...slide, elementList: newElementList } : slide
+  );
+
+  const newPresentation = { ...editor.presentation, slideList: newSlideList };
+
+  const newEditor = { ...editor, newPresentation};
+
+  return newEditor;
+}
+
+export function deleteFilter(
+  editor: Editor,
+  slideId: number,
+  element: Element
+): Editor {
+  //const data = element.data as Image;
+  const data = element.image;
+  const newData = { ...data, filter: {} };
+  const newElement = { ...element, data: newData };
+
+  const { slideList } = editor.presentation;
+  const { elementList } = slideList[slideId];
+
+  const newElementList = elementList.map((element, index) =>
+    index === newElement.id ? newElement : element
+  );
+
+  const newSlideList = slideList.map((slide, index) =>
+    index === slideId ? { ...slide, elementList: newElementList } : slide
+  );
+
+  const newPresentation = { ...editor.presentation, slideList: newSlideList };
+
+  const newEditor = { ...editor, newPresentation};
+
+  return newEditor;
+}
 
 // export function loadImage(
 //   editor: Editor,
