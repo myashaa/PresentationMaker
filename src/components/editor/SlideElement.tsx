eb5ac74906e6a0f83eb3fee712b7da5ce42a927d
Konstantin <k.podyganov@mail.ru>
2021-12-03 01:50:07 +0300

    #17 Added useResize

--- src/components/editor/SlideElement.tsx
+++ src/components/editor/SlideElement.tsx
@@ -1,6 +1,7 @@
 import { RefObject, useEffect, useRef, useState } from "react";
 import { dispatch } from "../../editor";
 import { useDragAndDrop } from "../../hooks/useDragAndDrop";
+import { useResize } from "../../hooks/useResize";
 import { Element as ElementType } from "../../model/element/ElementTypes";
 import { setText } from "../../model/element/TextActions";
 import { moveElement } from "../../model/slide/SlideActions";
@@ -27,14 +28,31 @@ export function SlideElement({
   const [isMulti, setMuli] = useState(false);
 
   const elementRef = useRef(null);
+  const tlRef = useRef(null);
+  const trRef = useRef(null);
+  const blRef = useRef(null);
+  const brRef = useRef(null);
 
-  const position = useDragAndDrop(elementRef, element.position, (state) => {
-    setIsMoving(state);
-    state && onClick && onClick(isMulti);
-  });
+  // const position = useDragAndDrop(elementRef, element.position, (state) => {
+  //   setIsMoving(state);
+  //   state && onClick && onClick(isMulti);
+  // });
+
+  const size = useResize(
+    elementRef,
+    element.position,
+    {
+      width: element.width,
+      height: element.height,
+    },
+    tlRef,
+    trRef,
+    blRef,
+    brRef
+  );
 
   useEffect(() => {
-    selected && dispatch(moveElement, slideId, id, position);
+    // selected && dispatch(moveElement, slideId, id, position);
   }, [isMoving]);
 
   return (
@@ -43,10 +61,12 @@ export function SlideElement({
         isMoving && styles.moving
       }`}
       style={{
-        width: element.width < 0 ? "auto" : element.width,
-        height: element.height < 0 ? "auto" : element.height,
-        top: position.y,
-        left: position.x,
+        // width: element.width < 0 ? "auto" : element.width,
+        // height: element.height < 0 ? "auto" : element.height,
+        width: size.width,
+        height: size.height,
+        top: element.position.y,
+        left: element.position.x,
       }}
       onClick={(event) => {
         event.stopPropagation();
@@ -57,10 +77,10 @@ export function SlideElement({
     >
       {selected && (
         <>
-          <span className={`${styles.resizer} ${styles.lt}`} />
-          <span className={`${styles.resizer} ${styles.lb}`} />
-          <span className={`${styles.resizer} ${styles.rt}`} />
-          <span className={`${styles.resizer} ${styles.rb}`} />
+          <span ref={tlRef} className={`${styles.resizer} ${styles.lt}`} />
+          <span ref={blRef} className={`${styles.resizer} ${styles.lb}`} />
+          <span ref={trRef} className={`${styles.resizer} ${styles.rt}`} />
+          <span ref={brRef} className={`${styles.resizer} ${styles.rb}`} />
         </>
       )}
       {element?.text && (
