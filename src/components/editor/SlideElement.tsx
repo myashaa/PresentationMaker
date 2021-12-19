db1d9dd82d30cd96472d4b0b30db5026ca525c1c
Masha <mianto017@mail.ru>
2021-12-19 13:07:07 +0300

    Добавила фигуры для отрисовки на слайде

--- src/components/editor/SlideElement.tsx
+++ src/components/editor/SlideElement.tsx
@@ -14,10 +14,11 @@ export function SlideElement({ element, selected, onClick }: ElementProps) {
 
   const isText = element.data.hasOwnProperty("font");
   const isImage = element.data.hasOwnProperty("url");
+  const isFigure = element.data.hasOwnProperty("type");
 
   const style = {
-    width: element.width < 0 ? "auto" : element.width,
-    height: element.height < 0 ? "auto" : element.height,
+    width: element.width < 0 ? "100px" : element.width,
+    height: element.height < 0 ? "100px" : element.height,
     top: element.position.y,
     left: element.position.x,
   };
@@ -43,6 +44,7 @@ export function SlideElement({ element, selected, onClick }: ElementProps) {
       {!isText && selected && resizers}
       {isText && <p>{element?.data?.content}</p>}
       {isImage && <img src={element.data.url} alt="" />}
+      {isFigure && <div className={classnames(styles.figure, styles[element.data?.type])}></div>}
     </div>
   );
 }
