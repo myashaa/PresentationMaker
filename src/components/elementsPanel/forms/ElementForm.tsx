b9664a3a8dacc3d4475dcb9a8bbb4fe639bf7817
Masha <mianto017@mail.ru>
2021-12-24 00:47:16 +0300

    Сверстала кнопку удаления элемента.

--- src/components/elementsPanel/forms/ElementForm.tsx
+++ src/components/elementsPanel/forms/ElementForm.tsx
@@ -1,3 +1,4 @@
+import { flattenDiagnosticMessageText } from "typescript";
 import { dispatch } from "../../../editor";
 import {
   changeElementBorder,
@@ -11,10 +12,18 @@ import {
 import { moveElement, resizeElement } from "../../../model/slide/SlideActions";
 import { FieldInput } from "../../fields/FieldInput";
 import { FieldSelect } from "../../fields/FieldSelect";
+import { ActionButton } from "../../header/actions/ActionButton";
 
 type Props = {
   element?: TElement;
   slideId?: string;
+};
+
+const style = {
+  marginRight: 0,
+  width: "100%",
+  display: "flex",
+  justifyContent: "center"
 };
 
 export function ElementForm({ element, slideId }: Props) {
@@ -117,6 +126,12 @@ export function ElementForm({ element, slideId }: Props) {
         }
         value={element?.color?.toUpperCase()}
       />
+      <ActionButton
+        icon="delete"
+        label={"Удалить элемент"}
+        style={style}
+          //onClick={() => dispatch(clearBackground, true, slide?.id)}
+        />
     </>
   );
 }
