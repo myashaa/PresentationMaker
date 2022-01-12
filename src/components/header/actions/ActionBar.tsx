8267171a55e34660fc4825b7a9413d4ecee41c67
Konstantin <k.podyganov@mail.ru>
2022-01-12 03:19:14 +0300

    added actionbar buttons

--- src/components/header/actions/ActionBar.tsx
+++ src/components/header/actions/ActionBar.tsx
@@ -26,88 +26,81 @@ function ActionBar({ newSlide, deleteSlides }: Props) {
           onClick={() => {
             deleteSlides();
           }}
+          style={{ marginRight: 24 }}
         />
-      </div>
-      {/* <div className={styles.appActionsGroup}>
-        
-        
+
         <ActionButton
           icon="undo"
           onClick={() => {
-            dispatch(undo, false, editor);
+            // dispatch(undo, false, editor);
           }}
         />
         <ActionButton
           icon="redo"
           onClick={() => {
-            dispatch(redo, false, editor);
+            // dispatch(redo, false, editor);
           }}
+          style={{ marginRight: 24 }}
         />
 
-        <div className={styles.appActionsRigth}>
-          <ActionButton
-            icon="text_fields"
-            onClick={() => {
-              const newText: TText = {
-                text: "Sample Text",
-                font: {
-                  family: "Montserrat",
-                  size: 16,
-                  color: COLORS.black,
-                  bold: false,
-                  underline: false,
-                  italic: false,
-                },
-              };
-              dispatch(createElement, true, selectedSlide, newText);
-            }}
-          />
-          <ActionButton
-            icon="image"
-            onClick={() => {
-              const fileInputNode = document.createElement("input");
-              fileInputNode.type = "file";
-              fileInputNode.click();
-              fileInputNode.addEventListener("change", () => {
-                const file = fileInputNode.files?.[0] as File;
-                const reader = new FileReader();
-
-                reader.onloadend = function () {
-                  const newImage: TImage = {
-                    image: "https://via.placeholder.com/150",
-                  };
-
-                  if (file.type.includes("image")) {
-                    newImage.image = String(reader.result);
-                  }
-
-                  dispatch(createElement, true, selectedSlide, newImage);
-                };
-
-                reader.readAsDataURL(file);
-              });
-            }}
-          />
-          <ActionButton
-            icon="category"
-            onClick={() => {
-              const newFigure: TFigure = {
-                figure: EFigureType.triangle,
-                fill: COLORS.primary,
-              };
-              dispatch(createElement, true, selectedSlide, newFigure);
-            }}
-          />
-          <ActionButton
-            icon="photo_camera"
-            onClick={() => {
-              const newCanvas: TCanvas = {
-                video: true,
-              };
-              dispatch(createElement, true, selectedSlide, newCanvas);
-            }}
-          />
-        </div>
+        <ActionButton
+          icon="text_fields"
+          onClick={() => {
+            // const newText: TText = {
+            //   text: "Sample Text",
+            //   font: {
+            //     family: "Montserrat",
+            //     size: 16,
+            //     color: COLORS.black,
+            //     bold: false,
+            //     underline: false,
+            //     italic: false,
+            //   },
+            // };
+            // dispatch(createElement, true, selectedSlide, newText);
+          }}
+        />
+        <ActionButton
+          icon="image"
+          onClick={() => {
+            // const fileInputNode = document.createElement("input");
+            // fileInputNode.type = "file";
+            // fileInputNode.click();
+            // fileInputNode.addEventListener("change", () => {
+            //   const file = fileInputNode.files?.[0] as File;
+            //   const reader = new FileReader();
+            //   reader.onloadend = function () {
+            //     const newImage: TImage = {
+            //       image: "https://via.placeholder.com/150",
+            //     };
+            //     if (file.type.includes("image")) {
+            //       newImage.image = String(reader.result);
+            //     }
+            //     dispatch(createElement, true, selectedSlide, newImage);
+            //   };
+            //   reader.readAsDataURL(file);
+            // });
+          }}
+        />
+        <ActionButton
+          icon="category"
+          onClick={() => {
+            // const newFigure: TFigure = {
+            //   figure: EFigureType.triangle,
+            //   fill: COLORS.primary,
+            // };
+            // dispatch(createElement, true, selectedSlide, newFigure);
+          }}
+        />
+        <ActionButton
+          icon="photo_camera"
+          onClick={() => {
+            // const newCanvas: TCanvas = {
+            //   video: true,
+            // };
+            // dispatch(createElement, true, selectedSlide, newCanvas);
+          }}
+        />
       </div>
 
       <ActionButton
@@ -115,9 +108,9 @@ function ActionBar({ newSlide, deleteSlides }: Props) {
         label="Слайд-шоу"
         primary
         onClick={() => {
-          dispatch(changeMode, true, EMode.view);
+          // dispatch(changeMode, true, EMode.view);
         }}
-      /> */}
+      />
     </div>
   );
 }
